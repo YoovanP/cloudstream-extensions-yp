910e4dc9c783e64efd8fc8cce1faadadd4343da9
YoovanP <yoovanp@github.com>
2026-04-18 09:54:19 +0530

    fix: update repo URL to cloudstream-extensions-yp

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/YoovanP/custom-extensions")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/YoovanP/cloudstream-extensions-yp")
         authors = listOf("YoovanP")
     }
 
